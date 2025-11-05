3c49053343fc21fbf1ad078acdb8f874ebe920b8
AipuAmeh <aipuameh.16@gmail.com>
2025-11-04 18:35:35 -0800

    created outline for starting question 1

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-# this folder will be for gathering and creating 
-# will also create a jupyter notebook with similar charts 
-# but main chart will be on tableau?
+
+
+# link to csv or db?
+# create python script that reflects information that should be retrieved by sql statement
+# after, create jupyter notebook with visualizations and plotly
+# after create tableau visualization using sql statement to outline what i should be looking for
+
 
