5dbfaedd0b535150ed39333f814b240a43a70cac
AipuAmeh <aipuameh.16@gmail.com>
2025-12-04 13:02:23 -0800

    created database connection, added first question query, ignored database; now i can use pivot tables

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+import pandas as pd
+import sqlite3
 
-
-# link to csv or db?
+# link to db
+db_path = 'new-db.db'
+conn = sqlite3.connect(db_path)
 # create python script that reflects information that should be retrieved by sql statement
 # after, create jupyter notebook with visualizations and plotly
 # after create tableau visualization using sql statement to outline what i should be looking for
 
 
+## step 1: get data
+sql_query_one = """
+SELECT * 
+FROM StagingHealthData
+;
+"""
+
+df_result = pd.read_sql(sql_query_one, conn)
+
+# 4. Close the connection
+conn.close()
+
+# Print the resulting DataFrame
+print(df_result.head(5))
